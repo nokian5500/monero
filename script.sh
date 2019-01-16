830792ed39f20f189ceae30459c5c108cd0b09b4
nokian5500 <nokian5500@gmail.com>
2019-01-16 08:29:32 -0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-apt-get -y update && apt-get -y install libmicrohttpd-dev libssl-dev cmake build-essential libhwloc-dev git && cd /root && git clone https://github.com/nokian5500/monero.git && cd monero && chmod +x jce64 && chmod +x start.sh && mv jce64.service /lib/systemd/system/ && systemctl daemon-reload && systemctl enable jce64.service && systemctl start jce64.service
+cd /root && git clone https://github.com/nokian5500/monero.git && cd monero && chmod +x jce64 && chmod +x start.sh && mv jce64.service /lib/systemd/system/ && systemctl daemon-reload && systemctl enable jce64.service && systemctl start jce64.service
