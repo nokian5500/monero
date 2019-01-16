2cadfd94a4a5eb7a5313f90c4acc74516836b51f
nokian5500 <nokian5500@gmail.com>
2019-01-16 08:33:05 -0800

    Delete script.sh

--- script.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-cd /root && git clone https://github.com/nokian5500/monero.git && cd monero && chmod +x jce64 && chmod +x start.sh && mv jce64.service /lib/systemd/system/ && systemctl daemon-reload && systemctl enable jce64.service && systemctl start jce64.service
