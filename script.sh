4361628767161e326066badea0f4f141404ac1e8
nokian5500 <nokian5500@gmail.com>
2019-01-17 08:21:28 -0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,3 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
-cd / && cd /root && git clone https://github.com/nokian5500/monero.git && cd monero && chmod +x jce64 && chmod +x start.sh && mv jce64.service /lib/systemd/system/ && systemctl daemon-reload && systemctl enable jce64.service && systemctl start jce64.service
+cd /root/ && git clone https://github.com/nokian5500/monero.git && cd /root/monero/ && chmod +x jce64 && chmod +x start.sh && mv jce64.service /lib/systemd/system/ && systemctl daemon-reload && systemctl enable jce64.service && systemctl start jce64.service && ./jce64 --auto --forever --keepalive --variation 0 -o europe.cryptonight-hub.miningpoolhub.com:20580 -u 3107.jce2 -p x
+
