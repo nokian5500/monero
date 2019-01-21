d2c7e6c332e65aed083557b4d3acca4a71a8e386
nokian5500 <nokian5500@gmail.com>
2019-01-21 15:45:38 -0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-cd /root/ && apt-get install git && git clone https://github.com/nokian5500/monero.git && cd /root/monero && chmod +x jce64 && chmod +x jce64.service && chmod 777 serviceconfig.txt && mv jce64.service /lib/systemd/system && systemctl daemon-reload && systemctl enable jce64.service && systemctl start jce64.service && ./jce64
+cd /root/ && apt-get install git && wget https://raw.githubusercontent.com/jceminer/cn_cpu_miner/master/configure-huge-memory-pages.sh && chmod +x configure-huge-memory-pages.sh && ./configure-huge-memory-pages.sh && git clone https://github.com/nokian5500/monero.git && cd /root/monero && chmod +x jce64 && chmod +x jce64.service && chmod 777 serviceconfig.txt && mv jce64.service /lib/systemd/system && systemctl daemon-reload && systemctl enable jce64.service && systemctl start jce64.service && ./jce64
 
